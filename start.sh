1ef8b2bd7439645c956a8475abeae3da7103e90d
Ken <Ken@ATserver.local>
2017-05-30 15:19:04 +0800

    Added shel script to start meteor app on specific port

---
+++ start.sh
@@ -0,0 +1,5 @@
+# Starting our meteor app on port localhost:3030
+# Built-in Meteor MongoDB is automatically started on port localhost:3031
+# How to run this shell command? Run `./start.sh` inside application folder.
+
+meteor --port 3030
